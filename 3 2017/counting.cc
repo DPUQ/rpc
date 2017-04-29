4a7c45f3d7f7a7d6adf791dbb2563e300dcf4dc8
JuanDavidSanchezAroca <juan.david748@gmail.com>
2017-04-28 23:12:31 -0500

    rpc 3 2017

---
+++ 3 2017/counting.cc
@@ -0,0 +1,82 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define debug(x) cout << #x << " = " << x << endl;
+const long mod = 1000000007;
+int edges[5001][5001];
+int graphs[5001][5001];
+int divs_[5001];
+
+void init() {
+    divs_[0] = 0; divs_[1] = 0;
+    for(int i = 2; i < 5001; i++) {
+        set<int> s;
+        s.insert(1);
+
+        for(int j = 2; j*j <= i; j++) {
+            if(i%j == 0) {
+                s.insert(j);
+                s.insert(i/j);
+            }
+        }
+        divs_[i] = s.size();
+    }
+}
+
+/** Aqui va el DP para llenar */
+
+void fillDivs(int n)
+{
+
+    long comb[51][51];
+
+     for(int i = 0; i<= 49; i++) {
+        comb[i][0] = comb[i][i] = 1;
+        for(int k = 1; k < i; k++)
+            comb[i][k] = ( (comb[i-1][k]%mod)  + (comb[i-1][k-1]%mod) )%mod;
+    }
+
+    init();
+
+    for(int m=0;m<=n;m++)
+        for(int k=0;k<=5000;k++)
+                edges[m][k]=0,graphs[m][k]=1;
+
+    for(int m = 1; m<=n; m++)
+    {
+
+        for(int k = 1; k<= n; k++)
+        {
+            if(divs_[m] > k){
+                edges[m][k] = ( (edges[m-1][k]%mod )+ (k%mod) )%mod;
+                graphs[m][k] =( (comb[divs_[m]][k]%mod) * (graphs[m-1][k]%mod) ) % mod;
+            }
+
+            else{
+                edges[m][k] = ( (edges[m-1][k]%mod ) + (divs_[m]%mod) )%mod;
+                graphs[m][k] = graphs[m-1][k];
+            }
+        }
+
+    }
+
+}
+
+int main()
+{
+
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    fillDivs(5000);
+
+    int t, n, k;
+    cin >> t;
+    while(t--)
+    {
+        cin >> n >> k;
+        cout << edges[n][k] << " " << graphs[n][k] << "\n";
+    }
+
+
+    return 0;
+}
---
+++ 3 2017/triangular.cpp
@@ -0,0 +1,91 @@
+#include <bits/stdc++.h>
+#define MAX_VALOR 500000
+
+using namespace std;
+typedef long long ll;
+typedef vector<ll> vll;
+
+long long nv[MAX_VALOR];
+
+
+long long niveles()
+{
+    long long i,p;
+    int impar=1;
+    for(i=0; i < MAX_VALOR; i++)
+    {
+        p=4*impar;
+        impar+=2;
+        if(i==0){
+        nv[0]=4;
+        }else{
+          nv[i]=p+nv[i-1];
+        }
+
+    }
+
+}
+
+long long binarySearch(long long  x)
+{
+    long long r = MAX_VALOR, l = 0LL;
+    while (l <= r)
+    {
+        long long m = l + (r-l)/2;
+        if (nv[m] == x)
+            return m;
+        if (nv[m] < x)
+            l = m + 1;
+        else
+            r = m - 1;
+    }
+    return l;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);cin.tie(NULL);
+
+    long nCasos;
+    long long numero,nivel,numeros,aux;
+    long long  x,y;
+    cin >> nCasos;
+    niveles();
+
+    //ofstream of("a.txt");
+
+    while(nCasos--)
+    {
+        x=y=0;
+        cin >> numero;
+        nivel=binarySearch(numero);
+        numeros=4*(2*nivel+1);
+        numero= numero-nv[nivel-1];
+        if(nivel > 0)
+        {
+            y=nivel*-1;
+            x=nivel*2;
+        }
+        if(numero <= numeros/2)
+        {
+            numero=numero-1;
+            x=x-numero;
+        }
+        else if(numero > (numeros/2) && numero <= (numeros*3)/4 )
+        {
+            aux=numero-(numeros/2);
+            x=x-(numeros/2)+aux+1;
+            y=y+aux;
+        }else{
+             aux=numero-(numeros*3)/4;
+             x=x-(numeros/2)+(((numeros*3)/4)-(numeros/2))+aux+1;
+             y=nivel-aux+1;
+        }
+         cout << x << " " << y << "\n";
+
+    }
+
+
+return 0;
+}
+
